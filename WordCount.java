d1d14a3c3e33094d2ed96f1e11a0763bec28a2ef
Asaduddin Bilal Mohammed <asaduddin1199@gmail.com>
2017-10-11 17:39:52 +0100

    Create WordCount.java

---
+++ WordCount.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+  public static void runJob(String[] input, String output) throws Exception {
+
+        Configuration conf = new Configuration();
+
+    Job job = new Job(conf);
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setNumReduceTasks(3);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    Path outputPath = new Path(output);
+    FileInputFormat.setInputPaths(job, StringUtils.join(input, ","));
+    FileOutputFormat.setOutputPath(job, outputPath);
+    outputPath.getFileSystem(conf).delete(outputPath,true);
+    job.waitForCompletion(true);
+  }
+
+  public static void main(String[] args) throws Exception {
+       runJob(Arrays.copyOfRange(args, 0, args.length-1), args[args.length-1]);
+  }
+
+}
+
